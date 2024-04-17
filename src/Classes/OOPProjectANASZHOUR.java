f0278b0fe5725baf463f8ab78325e8a57e4025c4
ANAS AL-ZHOUR <anaszhoor.z2016@gmail.com>
2024-04-17 21:36:59 +0200

    someEditingOnMain

--- src/Classes/OOPProjectANASZHOUR.java
+++ src/Classes/OOPProjectANASZHOUR.java
@@ -16,16 +16,27 @@ public class OOPProjectANASZHOUR {
     public static void main(String[] args) {
         Ingredient i1 = new Fruit("Mango" , new Color(15 , 40 ,67) , 50 , 20);
         Ingredient i2 = new Fruit("Kiwi" , new Color(19 , 46 ,17) , 28 , 18);
-        Ingredient i3 = new Milk(new Color(0 , 0 , 0) , 17 , 36);
+        Ingredient i3 = new Fruit("لآanana" , new Color(57 , 34 ,16) , 40 , 41);
+        Ingredient i4 = new Milk(new Color(0 , 0 , 0) , 17 , 36);
+        Ingredient i5 = new Sugar(3);
+        
+        
         
         Blender b1 = new Blender(800);
+        
         b1.addIngredient(i1);
         b1.addIngredient(i2);
         b1.addIngredient(i3);
+        b1.addIngredient(i4);
+        b1.addIngredient(i5);
+        
+        Cup c1 = new Cup(16);
+        
+        
         
         b1.blend();
         
-        System.out.println(b1.pourIntoCup(new Cup(15)));
+        System.out.println(b1.pourIntoCup(c1));
         
         
         
